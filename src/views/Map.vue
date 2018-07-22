910868d6f9be340a3d1e9926c85fd5657e625a34
Eduard Cot Ros <eduard@Toc-Toc-computer.local>
2018-07-22 14:34:14 +0200

    buttons

---
+++ src/views/Map.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="map">
+    <h1>Map</h1>
+
+    <div id="tools">
+        <input type="text" id="initialTime" value="18" class="small-input"></input>h.
+        <button onclick="changeInitialTime()">Reinicia</button>
+        <button onclick="start()">Start</button>
+        <button onclick="pause()">Pause</button>
+    </div>
+    
+  </div>
+</template>
+
+<style>
+  .small-input{
+    width:15px;
+  }
+</style>
+
